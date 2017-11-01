f9ec55c8e8e8a9a9da6c80832463096fcd6ad3f1
Emmet <emmetcooke@gmail.com>
2017-11-01 11:56:49 -0500

    Defined the Node class

--- Lab6/Node.cpp
+++ Lab6/Node.cpp
@@ -1,6 +1,54 @@
 /*************************************************
 * Author: Emmet Cooke
 * Date: 10/31/2017
-* Description: 
+* Description: This file houses the definitions
+* for the functions of the Node class. 
 *************************************************/
 #include "Node.hpp"
+
+/*************************************************
+* Description: Default Constructor.
+*************************************************/
+Node::Node()
+{
+	setNum(0);
+	prev = nullptr;
+	next = nullptr;
+}
+
+/*************************************************
+* Description: Destructor. Frees memory held by prev
+* and next.
+*************************************************/
+Node::~Node()
+{
+	delete prev;
+	delete next;
+}
+
+/*************************************************
+* Description: Sets num equal to the number passed
+* in.
+*************************************************/
+void Node::setNum(int numIn)
+{
+	num = numIn;
+}
+
+/*************************************************
+* Description: Sets the next pointer to the Node
+* passed in.
+*************************************************/
+void Node::setNext(Node* nextNode)
+{
+	next = nextNode;
+}
+
+/*************************************************
+* Description: Sets the prev pointer to the Node
+* passed in.
+*************************************************/
+void Node::setPrev(Node* prevNode)
+{
+	prev = prevNode;
+}
--- Lab6/Node.hpp
+++ Lab6/Node.hpp
@@ -1,7 +1,8 @@
 /*************************************************
 * Author: Emmet Cooke
 * Date: 10/31/2017
-* Description:
+* Description: This is the header file for the Node
+* class.
 *************************************************/
 #ifndef NODE_HPP
 #define NODE_HPP
@@ -17,14 +18,14 @@ public:
 	~Node();
 
 	// Getters
-	int getNum() const;
-	Node * getPrev() const;
-	Node * getNext() const;
+	int getNum() const { return num; }
+	Node * getPrev() const { return prev; }
+	Node * getNext() const { return next; }
 	
 	// Setters
-	void setNum();
-	void setPrev();
-	void setNext();
+	void setNum(int numIn);
+	void setPrev(Node * prevNode);
+	void setNext(Node * nextNode);
 };
 
 #endif
